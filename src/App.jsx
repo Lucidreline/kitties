11ef522920f8c86ce35e6f685546eaddcaeb2ba8
Manny <casta.ma502@gmail.com>
2025-08-02 23:03:44 -0700

    Flyers will now come with their own url path.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import catDetails from './cat-details'
 import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+
 import Flyer from './components/flyer/flyer'
 
 function App() {
   return (
     <div className="flyers">
-      {catDetails.map(detail => (
-        <Flyer name={detail.name} pronounce={detail.pronounce} details={detail.specifics} photo={detail.photo}></Flyer>
-      ))}
+      <Routes>
+
+        {catDetails.map(detail => (
+          <Route path={`/${detail.name}`} element={<Flyer name={detail.name} pronounce={detail.pronounce} details={detail.specifics} photo={detail.photo}></Flyer>} />
+        ))}
+      </Routes>
     </div>
 
   )
