77fcd4eaaed2bd0d602444653ef5db36b2a8c890
Manny <casta.ma502@gmail.com>
2025-08-02 23:09:10 -0700

    Added in a home page starter.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 
 import Flyer from './components/flyer/flyer'
+import HomePage from './components/home-page/home-page'
 
 function App() {
   return (
     <div className="flyers">
       <Routes>
-
+        <Route path="/" element={<HomePage />} />
         {catDetails.map(detail => (
           <Route path={`/${detail.name}`} element={<Flyer name={detail.name} pronounce={detail.pronounce} details={detail.specifics} photo={detail.photo}></Flyer>} />
         ))}
