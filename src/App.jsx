daafb050279fcf38123d3ed88c9f12a8930a54a8
Manny <casta.ma502@gmail.com>
2025-08-02 22:56:25 -0700

    Started making the flyers more scalable, data in another file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { useState } from 'react'
-
+import catDetails from './cat-details'
 import './App.scss'
 import Flyer from './components/flyer/flyer'
 
 function App() {
-  const endymionDetails = [
-    { normal: "Last Seen: ", accent: "08/02/2025" },
-    { normal: "Wearing: ", accent: "A blue color with fishes" },
-    { normal: "Super shy, approach slowly", accent: "" },
-    { normal: "Call him by name in a soft tone", accent: "" },
-    { normal: "Responds to ", accent: `"Come here Endymion"` },
-    { normal: "Location: ", accent: "Shady Dell Ln & Knapps" }
-  ]
   return (
-    <Flyer name="Endymion" pronounce="N-Dim-E-N" details={endymionDetails} photo="endymion.jpg"></Flyer>
+    <div className="flyers">
+      {catDetails.map(detail => (
+        <Flyer name={detail.name} pronounce={detail.pronounce} details={detail.specifics} photo={detail.photo}></Flyer>
+      ))}
+    </div>
 
   )
 }
---
+++ src/cat-details.js
@@ -0,0 +1,15 @@
+const catDetails = [
+    {
+        name: "Endymion",
+        pronounce: "N-Dim-E-N",
+        photo: "endymion.jpg",
+        specifics: [{ normal: "Last Seen: ", accent: "08/02/2025" },
+        { normal: "Wearing: ", accent: "A blue color with fishes" },
+        { normal: "Super shy, approach slowly", accent: "" },
+        { normal: "Call him by name in a soft tone", accent: "" },
+        { normal: "Responds to ", accent: `"Come here Endymion"` },
+        { normal: "Location: ", accent: "Shady Dell Ln & Knapps" }]
+    }
+]
+
+export default catDetails
