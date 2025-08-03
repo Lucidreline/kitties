139c2a5ea01da676eb700633eed68f9f02716eab
Manny <casta.ma502@gmail.com>
2025-08-02 23:58:41 -0700

    Added a cmd style home page.

--- src/components/home-page/home-page.jsx
+++ src/components/home-page/home-page.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import catDetails from '../../cat-details'
+import { Link } from 'react-router-dom'
+
+import './home-page.scss'
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>You are home!!</h1>
+        <div className="home">
+            <div className="terminal-text">
+                <p>Mannysoft Windows [Version 10.0.19045.6093]</p>
+                <p>(c) Mannysoft Corporation. All rights reserved.</p>
+                <br />
+                <p>C:\Users\Manny dir</p>
+                <br />
+                {catDetails.map(detail => (
+                    <Link className="cat-links" key={detail.name} to="/Endymion">
+                        <span className="date">{detail.birthDate}</span>
+                        <span className="time">{detail.birthTime}</span>
+                        <span className="byte-size">{detail.byteSize}</span>
+                        <span className="file-name">{detail.name}</span>
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
